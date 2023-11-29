b790317c1665bdafea085828a10bc9678294fc52
Mateusz <ryniuss@o2.pl>
2023-11-29 14:27:10 +0100

    add view amount in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 import React from 'react';
 
 const Header = () => {
+	const { cartItems } = useSelector((state) => state.cart);
+	console.log(cartItems);
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -23,6 +25,11 @@ const Header = () => {
 								<Nav.Link>
 									<FaShoppingCart />
 									Cart
+									{cartItems.length > 0 && (
+										<Badge pill bg='success' style={{ marginLeft: '5px' }}>
+											{cartItems.reduce((a, c) => a + c.qty, 0)}
+										</Badge>
+									)}
 								</Nav.Link>
 							</LinkContainer>
 							<LinkContainer to='/login'>
